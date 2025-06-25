1fbecc66b81d352f409c5e01143266fcca2661dc
Karl Isenberg <kisenber@amd.com>
2025-06-25 14:35:51 -0700

    Fix DNS lookup

--- scripts/users/create-kubeconfig.sh
+++ scripts/users/create-kubeconfig.sh
@@ -17,17 +17,18 @@ KUBECONFIG_FILE="$KUBECONFIG_DIR/$USER_NAME.conf"
 
 if [[ -z "${API_SERVER:-}" ]]; then
   DNS_ALL="$(openssl x509 -in "$API_CERT" -noout -text | grep "DNS:" | tr ',' '\n' | grep "DNS:" | tr -d 'DNS: ')"
-  if DNS_NLB="$(echo "$DNS_ALL" | grep '.amazonaws.com')"; then
-    # AWS NLB
-  elif DNS_NLB="$(echo "$DNS_ALL" | grep '.lb.appdomain.cloud')"; then
-    # IBM LB
+  if API_DNS="$(echo "$DNS_ALL" | grep '.amazonaws.com')"; then
+    : # AWS
+  elif API_DNS="$(echo "$DNS_ALL" | grep '.lb.appdomain.cloud')"; then
+    : # IBM
   else
     echo >&2 "DNS not found in ${API_CERT}:"
     echo >&2 "${DNS_ALL}:"
     exit 1
   fi
   # TODO: Lookup API port
-  API_SERVER="https://${DNS_NLB}:6443"
+  API_SERVER="https://${API_DNS}:6443"
+  echo "API Server: ${API_SERVER}:"
 fi
 
 # Base64 encode the certificates
